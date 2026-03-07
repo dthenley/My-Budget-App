cdf967129bd68924e4370b49eea12370c34cfb00
Donte Henley <3503886+dthenley@users.noreply.github.com>
2026-03-06 20:54:00 -0600

    Add multi-step form for debt and income input with navigation controls

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -4,7 +4,10 @@ import { dashboard, login, register } from '@/routes';
 import DebtInput from './debtInput';
 
 export default function App() {
-  const [debtList, setDebtList] = useState([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]);
+    const [currentStep, setCurrentStep] = useState(0);
+    const [debtList, setDebtList] = useState([{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]);
+    const [incomeList, setIncomeList] = useState([{ id: 1 }, { id: 2 }]);
+
 
   const duplicate = () => {
     const newDebt = { id: debtList.length + 1 };
@@ -13,14 +16,45 @@ export default function App() {
 
   return (
     <div>
-        <button onClick={duplicate}>Duplicate</button>
         <form>
-            <ul>
-                {debtList.map((card) => (
-                    <DebtInput key={card.id} id={card.id} />
-                ))}
-            </ul>
-            <button type="submit">Save</button>
+            {currentStep === 0 && (
+                <>
+                <button onClick={duplicate}>Duplicate</button>
+                    <ul>
+                        {debtList.map((card) => (
+                            <DebtInput key={card.id} id={card.id} />
+                        ))}
+                    </ul>
+                    <button onClick={() => setCurrentStep(1)}>Next</button>
+                </>
+            )}
+            {currentStep === 1 && (
+                <>
+                    <button onClick={duplicate}>Duplicate</button>
+                    <ul>
+                        {incomeList.map((card) => (
+                            <li key={card.id} id={`income-${card.id}`}>
+                                <input type="text" placeholder="Income Name" />
+                                <input type="text" placeholder="Amount" />
+                                <select name="frequency" id="frequency">
+                                    <option value="weekly">Weekly</option>
+                                    <option value="bi-weekly">Every 2 Weeks</option>
+                                    <option value="first_and_15th">1st and 15th</option>
+                                    <option value="monthly">Every Month</option>
+                                </select>
+                                <input type="date" placeholder="Date of Next Payment" />
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={() => setCurrentStep(2)}>Next</button>
+                    <button onClick={() => setCurrentStep(0)}>Back</button>
+                </>
+            )}
+            {currentStep === 2 && (
+                <>
+
+                </>
+            )}
         </form>
     </div>
   );
